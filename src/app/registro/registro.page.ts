ddadcb842115bcbe849753077dca043b91aea28e
pedroferrarezi <60709746+pedroferrarezi@users.noreply.github.com>
2020-09-08 13:47:24 -0300

    banco de dados
    sql

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CpfValidator } from '../validators/cpf-validator';
 import { ComparacaoValidator } from '../validators/comparacao-validator';
+import { UsuariosService } from '../services/usuarios.service';
+import { AlertController } from '@ionic/angular';
+import { Usuario } from '../models/Usuario';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
@@ -51,7 +54,11 @@ export class RegistroPage implements OnInit {
 
 
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {
+  constructor(private formBuilder: FormBuilder,
+      private usuarioService: UsuariosService,
+      public alertController: AlertController,
+      public router: Router
+    ) {
     this.formRegistro = formBuilder.group({
       nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
       cpf: ['', Validators.compose([
@@ -75,7 +82,40 @@ export class RegistroPage implements OnInit {
 
   }
 
-  ngOnInit() {
+   async ngOnInit() {
+    await this.usuarioService.buscarTodos();
+    console.log(this.usuarioService.listaUsuarios);
   }
-    
+    public async salvarFormulario() {
+        if(this.formRegistro.valid) {
+          let usuario = new Usuario();
+          usuario.nome = this.formRegistro.value.nome;
+          usuario.cpf = this.formRegistro.value.cpf;
+          usuario.dataNascimento = new Date(this.formRegistro.value.dataNascimento);
+          usuario.genero = this.formRegistro.value.genero;
+          usuario.celular = this.formRegistro.value.celular;
+          usuario.email = this.formRegistro.value.email;
+          usuario.senha = this.formRegistro.value.senha;
+          if( await this.usuarioService.salvar(usuario)){
+            this.exibirAlerta('SUCESSO','USUARIO SALVO COM SUCESSO');
+            this.router.navigateByUrl('/login');
+
+          } else{
+            this.exibirAlerta('ERRO','ERRO AO  SALVAR O USUARIO');
+          }
+        }else{
+            this.exibirAlerta('ADVERTENCIA!','FORMULARIO INVALIDO<br>VERIFIQUE OS CAMPOS DO SEU FORMULARIO');
+        }
+    }
+
+    async exibirAlerta( titulo: string, mensagem: string) {
+      const alert = await this.alertController.create({
+        
+        header: titulo,
+        message: mensagem,
+        buttons: ['OK']
+      });
+  
+      await alert.present();
+    }
 }
---
+++ src/app/services/armazenamento.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArmazenamentoService {
+
+  constructor(private storage: Storage) { }
+
+  public salvarDados(chave: string, dados:any) {
+    if(chave.trim().length > 0 && dados) {
+      return this.storage.set(chave,dados)
+      .then(() => {
+        console.log('dados armazenados com sucesso');
+        return true;
+
+      })
+      .catch(erro => {
+        console.log('erro ao gravar os dados', erro);
+        return false;
+      });
+    } else {
+      return false;
+    } // fim do salvar dados
+ 
+
+ }
+    public PegarDados(chave:string) { 
+
+
+      if( chave.trim().length > 0 ) {
+        return this.storage.get(chave)
+        .then(dadosArmazenados => {
+          return dadosArmazenados;
+        }) 
+        .catch(erro => {
+          console.log('erro ao buscar os dados', erro);
+          return null;
+        });
+      } else {
+        return null;
+      }
+  }
+}
